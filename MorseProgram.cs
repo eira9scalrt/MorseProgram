64bdde53becdc2257aa88433adfbd360d9e9f9ed
eira <iwasawamasami_ggo@yahoo.co.jp>
2021-08-14 17:52:18 +0900

    初コミット

---
+++ MorseProgram.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+class Program{
+	public void Main(){
+		
+	}
+}
+
+class Basis{
+	public Basis(){
+		var m_data = new Dictionary<int,string>(){}
+	}
+}
---
+++ Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Product{
+	static Name;
+	int Id;
+	int Price;
+	public  Product(static _name,int _id,int, _price){
+		Name  = _name;
+		Id    = _id;
+		Price = _price;
+	}
+	public Show(){
+		Console.WriteLine( Name );
+		Console.WriteLine( Id );
+		Console.WriteLine( Price );
+	}
+}
