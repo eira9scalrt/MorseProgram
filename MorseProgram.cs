10969528b0ebf3e4b98f3592c76b7edadd631cb3
eira <iwasawamasami_ggo@yahoo.co.jp>
2021-08-14 18:23:26 +0900

    更新

--- MorseProgram.cs
+++ MorseProgram.cs
@@ -2,13 +2,26 @@ using System;
 using System.Collections.Generic;
 
 class Program{
-	public void Main(){
-		
+	public static void Main(){
+		Basis basis = new Basis();
+		basis.Show("A");
 	}
 }
 
 class Basis{
+	private Dictionary<string,string> m_data;
 	public Basis(){
-		var m_data = new Dictionary<int,string>(){}
+		m_data = new Dictionary<string,string>();
+		m_data.Add("A","・ー");	
+		m_data.Add("B","ー・・・");
+		m_data.Add("C","ー・ー・");
+	}
+	public void Show(string key){
+		if(m_data.ContainsKey(key)){
+			Console.WriteLine(m_data[key]);
+		}
+		else{
+			Console.WriteLine("エラー：存在しないKeyです");
+		}
 	}
 }
