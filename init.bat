1bfe590256424558c9328cc941586f7e3e02d621
syalpon <onoue0107@gmail.com>
2021-08-15 14:34:09 +0900

    bat fileの修正

--- init.bat
+++ init.bat
@@ -1,12 +1,24 @@
+@rem ---C#ランタイムのパスを通す
 path = C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 set str=%~nx1
+set exefile=%str:cs=exe%
+
+@rem --- 引数なしでバッチファイルを実行したとき
 @if "%1"=="" (
 start cmd \k
+@rem --- ファイルがドラッグされているとき
 ) else (
 @echo off
+@rem --- exeファイルの削除
+del %exefile%
+@rem --- コンパイル実行
 csc -nologo %str%
+@rem --- コンパイル成功時
+@if exist %exefile% (
 cls
-%str:cs=exe%
+%exefile%
+) else (
 pause >NUL
+)
 )
 
