329c3d6a36f93977575038242edd7248778afb6d
syalpon <onoue0107@gmail.com>
2021-08-15 18:12:41 +0900

    ダブルクリックが開かないバグ修正

--- init.bat
+++ init.bat
@@ -1,8 +1,7 @@
 @rem ---C#ランタイムのパスを通す
 path = C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 set str=%~nx1
-set exefile=%str:cs=exe%
-
+set exefile="%str:cs=exe%"
 @rem --- 引数なしでバッチファイルを実行したとき
 @if "%1"=="" (
 start cmd \k
